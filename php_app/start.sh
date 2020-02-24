c33aed57cf27198aa8071f58b3a61bf90df354d2
Hilmi Raditya <raditya113@gmail.com>
2020-02-25 00:03:27 +0700

    uncomment

--- php_app/start.sh
+++ php_app/start.sh
@@ -3,10 +3,3 @@ docker exec c_app composer install
 docker exec c_app cp .env.example .env
 docker exec c_app php artisan key:generate
 docker exec c_app chown -R www-data:www-data /var/www
-
-
-#hehe
-#hoho
-#jasasdawdsdfsadfsdasdsadsdsadssdadsdsa
-
-#bismillah
