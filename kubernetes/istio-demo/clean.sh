5969410f3ae5e02813ae3fc04aee5d387d34744a
Hilmi Raditya <raditya113@gmail.com>
2020-03-23 04:40:05 +0700

    hehe mantap lah

---
+++ kubernetes/istio-demo/clean.sh
@@ -0,0 +1,6 @@
+kubectl delete all --all -n default
+kubectl delete vs mydata-vs
+kubectl delete cm --all -n default
+kubectl delete gateways mydata-gateway
+kubectl delete svc --all -n default
+kubectl delete dr mydata
---
+++ kubernetes/istio-demo/testing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+curl -v "http://172.17.125.128/"
+sleep 1
+done
